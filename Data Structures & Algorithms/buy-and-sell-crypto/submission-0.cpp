841d15be4b92b68ad3463961d0e2632a84303ad4
ayushchoudhary-tech <ayushchoudhary.us18@gmail.com>
2026-07-25 14:13:55 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxprofit=0, bestBuy=INT_MAX;
+        for(int i=0;i<prices.size();i++)
+        {
+            if(prices[i] > bestBuy)
+            {
+                maxprofit=max(maxprofit,prices[i]-bestBuy);
+            }
+            bestBuy=min(bestBuy,prices[i]);
+        }
+        return maxprofit;
+    }
+};
