1bd575983fa6f6507eed378f553e42fe03679635
ayushchoudhary-tech <ayushchoudhary.us18@gmail.com>
2026-08-31 22:00:01 +0530

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public: 
+        void getAllSubset(vector<int>& nums,vector<int>&subset,int i,vector<vector<int>>&AllSubSet)
+        { if(i==nums.size())
+        {
+            AllSubSet.push_back(subset);
+            return;
+        } subset.push_back(nums[i]);
+          getAllSubset(nums,subset,i+1,AllSubSet);
+          subset.pop_back();
+          getAllSubset(nums,subset,i+1,AllSubSet);
+
+
+
+        }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>AllSubSet;
+        vector<int>subset;
+         getAllSubset(nums,subset,0,AllSubSet);
+         return AllSubSet;
+    }
+};
